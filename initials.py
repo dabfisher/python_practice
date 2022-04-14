99c64d4b40a362ede1b84293c1d79f5e92532b56
dabfisher <dabfisher@gmail.com>
2022-04-13 22:41:22 -0600

    ASCII initials

---
+++ initials.py
@@ -0,0 +1,11 @@
+string1 = "DDD     FFFF"
+string2 = "D  D    F   "
+string3 = "D   D   FFFF"
+string4 = "D  D    F   "
+string5 = "DDD     F   "
+
+print(string1)
+print(string2)
+print(string3)
+print(string4)
+print(string5)
