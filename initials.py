85a00bd54f1fcc0cd58e16f5bdda0020b2bf55f8
dabfisher <dabfisher@gmail.com>
2022-04-13 22:45:08 -0600

    name and fact add

--- initials.py
+++ initials.py
@@ -1,9 +1,14 @@
+first_last_name = "Danny Fisher"
+fun_fact = "I'm awesome"
+
 string1 = "DDD     FFFF"
 string2 = "D  D    F   "
 string3 = "D   D   FFFF"
 string4 = "D  D    F   "
 string5 = "DDD     F   "
 
+print(first_last_name)
+print(fun_fact)
 print(string1)
 print(string2)
 print(string3)
