3de584f8300a85042a6ff89758b609682f943b1e
dabfisher <dabfisher@gmail.com>
2022-06-14 19:51:03 -0600

    scrabble rename

--- scrabble.py
+++ scrabble.py
@@ -1,9 +1,16 @@
+from multiprocessing.sharedctypes import Value
+
+
 letters = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"]
-points = [1, 3, 3, 2, 1, 4, 2, 4, 1, 8, 5, 1, 3, 4, 1, 3, 10, 1, 1, 1, 1, 4, 4, 8, 4, 10]
+points = [1, 3, 3, 2, 1, 4, 2, 4, 1, 8, 5, 1, 3, 4, 1, 3, 69, 1, 1, 1, 1, 4, 4, 8, 4, 10]
 
 letter_to_points = {
-    letters:points 
-    for letters, points 
+    key: Value 
+    for key, value 
     in zip(letters, points)
     }
+print(letter_to_points)
+
+letter_to_points[" "] = 0
+
 print(letter_to_points)
